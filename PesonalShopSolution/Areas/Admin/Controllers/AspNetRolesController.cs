852f68f23619e39d51b90cdb03a52145a260c015
Tu <hoangtu1392001@gmail.com>
2021-05-23 10:24:52 +0700

    [Add]_feature/ManageUser

---
+++ PesonalShopSolution/Areas/Admin/Controllers/AspNetRolesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PesonalShopSolution.Areas.Admin.Data;
+using PesonalShopSolution.Areas.Admin.Models;
+
+namespace PesonalShopSolution.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AspNetRolesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AspNetRolesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AspNetRoles
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.AspNetRoles
+                .Include(a => a.AspNetUserRoles)
+                .ThenInclude(b => b.User)
+                .ToListAsync();
+            return View(result);
+        }
+
+        // GET: Admin/AspNetRoles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] AspNetRoles aspNetRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aspNetRoles);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aspNetRoles);
+        }
+
+
+
+        // GET: Admin/AspNetRoles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetRoles = await _context.AspNetRoles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aspNetRoles == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetRoles);
+        }
+
+        // POST: Admin/AspNetRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var aspNetRoles = await _context.AspNetRoles.FindAsync(id);
+            _context.AspNetRoles.Remove(aspNetRoles);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ PesonalShopSolution/Areas/Admin/Controllers/AspNetUserRolesController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PesonalShopSolution.Areas.Admin.Data;
+using PesonalShopSolution.Areas.Admin.Models;
+using PesonalShopSolution.ViewModels;
+
+namespace PesonalShopSolution.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AspNetUserRolesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AspNetUserRolesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Admin/AspNetUserRoles/Create
+        public async Task<IActionResult> Create(int roleId)
+        {
+            var role = await _context.AspNetUsers.Select(x => x).ToListAsync();
+
+            var create = new CreateRoleViewModel()
+            {
+                RoleId = roleId,
+                AspNetUsers = role
+            };
+
+            return View(create);
+        }
+
+        // POST: Admin/AspNetUserRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] int roleId, int userId)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new AspNetUserRoles
+                {
+                    RoleId = roleId,
+                    UserId = userId
+                };
+                var find = await _context.AspNetUserRoles.FirstOrDefaultAsync(x => x.RoleId == roleId && x.UserId == userId);
+                if (find != null)
+                {
+                    return RedirectToAction("Index", "AspNetRoles");
+                }
+                await _context.AspNetUserRoles.AddAsync(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "AspNetRoles");
+            }
+            return NotFound();
+        }
+
+
+        // GET: Admin/AspNetUserRoles/Delete/5
+        public async Task<IActionResult> Delete(int roleId, int userId)
+        {
+            var aspNetRoles = await _context.AspNetUserRoles
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.RoleId == roleId && m.UserId == userId);
+            if (aspNetRoles == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetRoles);
+        }
+
+
+        // POST: Admin/AspNetUserRoles/Delete/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int roleId, int userId)
+        {
+            var aspNetRoles = await _context.AspNetUserRoles
+                .Where(x => x.RoleId == roleId && x.UserId == userId)
+                .FirstOrDefaultAsync();
+            _context.AspNetUserRoles.Remove(aspNetRoles);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "AspNetRoles");
+        }
+
+    }
+}
